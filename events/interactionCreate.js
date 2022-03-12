cfbee7afc218a05c3b135ca1f2305b1841dba182
Erik Kindberg <52127288+erikkindberg@users.noreply.github.com>
2022-03-12 17:24:12 +0100

    Exclude GKs from Captain selection

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -150,7 +150,8 @@ module.exports = {
                     if (await matchmakingService.isMixOrCaptainsReadyToStart(lineup)) {
                         lineup = await teamService.startPicking(lineup.channelId)
 
-                        const allUserIds = lineup.roles.filter(role => role.user).map(role => role.user.id)
+                        const allUserIds = lineup.roles.filter(role => !role.name.includes('GK')).map(role => role.user.id)
+                        console.log()
                         let captainsIds = (await matchmakingService.findTwoMostRelevantCaptainsIds(allUserIds)).map(result => result._id)
                         if (captainsIds.length < 2) {
                             captainsIds = [allUserIds.splice(Math.floor(Math.random() * allUserIds.length), 1)[0], allUserIds.splice(Math.floor(Math.random() * allUserIds.length), 1)[0]]
